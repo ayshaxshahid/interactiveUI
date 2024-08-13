13cd10b65582614542182e39932492bce8f89307
aysha shahid <21ashah@whs.whsat.uk>
2024-08-13 09:42:58 +0100

    Initial Commit

---
+++ interactiveUI/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  interactiveUI
+//
+//  Created by Scholar on 08/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    @State var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack {
+            
+            Text(textTitle)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+            TextField("", text: $name)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.gray, width : 1)
+            
+            .padding()
+            
+            Button("Submit Name") {
+                textTitle = "Welcome \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+        }
+        .padding()
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Scholar on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
